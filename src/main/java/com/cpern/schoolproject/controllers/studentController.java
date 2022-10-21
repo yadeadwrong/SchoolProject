f18d7767c37b4650b63540d762b830d44686f20e
yadeadwrong <105574254+yadeadwrong@users.noreply.github.com>
2022-10-21 21:02:09 +0200

    version 1

--- src/main/java/com/cpern/schoolproject/controllers/studentController.java
+++ src/main/java/com/cpern/schoolproject/controllers/studentController.java
@@ -6,14 +6,12 @@ import com.cpern.schoolproject.repositories.studentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -22,17 +20,42 @@ public class studentController {
     @Autowired
     studentRepository studentRepository;
     @GetMapping("/studente")
-    public ResponseEntity<Page<student>> getAllStudents()
+    public ResponseEntity<Page<student>> getAllStudents(@RequestParam(defaultValue = "0") Integer page)
     {
-         Page<student>  studentPage = studentRepository.findAll(PageRequest.of(0,10));
-         return new ResponseEntity<Page<student>>(studentPage, HttpStatus.OK);
+            try {
+                Page<student> studentPage = studentRepository.findAll(PageRequest.of(page, 3));
+                return new ResponseEntity<>(studentPage, HttpStatus.OK);
+            } catch (Exception e) {
+                return ResponseEntity.internalServerError().build();
+            }
     }
+    @GetMapping("/studente/{id}")
+    public  ResponseEntity getSingleStudent(@PathVariable("id") long id){
+        try
+        {
+            Optional<student> retStudent;
+            retStudent = this.studentRepository.findById(id);
+            if(retStudent.isEmpty())
+            {
+                return ResponseEntity.notFound().build();
+            }
+            else {
+                return ResponseEntity.ok(retStudent);
+            }
+        }
+        catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
     @PostMapping("/studente/add")
     public ResponseEntity addStudent(@RequestBody  student studenteDaAgg)
     {
         studentRepository.save(studenteDaAgg);
-        return new ResponseEntity<String>("Studente Aggiunto", HttpStatus.OK);
+        return new ResponseEntity<>("Studente Aggiunto", HttpStatus.OK);
     }
+
+
 
 
 
